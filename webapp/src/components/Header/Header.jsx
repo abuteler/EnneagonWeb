1f918336fb614287901ddb6748f28a4ce359e7d0
Ale R. Buteler <alebuteler@gmail.com>
2020-03-01 17:08:17 -0300

    Renamed directory and upgraded dependencies.

---
+++ webapp/src/components/Header/Header.jsx
@@ -0,0 +1,45 @@
+import React from 'react';
+import { NavLink } from "react-router-dom";
+
+import Grid from '@material-ui/core/Grid';
+import { AccountBox, AccessTime } from '@material-ui/icons';
+
+import './Header.scss';
+import { ReactComponent as LogoTop } from '../../assets/img/logo_top.svg';
+import { ReactComponent as EnneagonStudios } from '../../assets/img/Typography9S.svg';
+
+class Header extends React.Component {
+  
+  render () {
+
+    return (
+      <header id="Header">
+        <Grid container justify="center">
+          <Grid item container xs={12} className="TopContainer" alignContent="center">
+            <Grid item xs={1} xl={2}></Grid>
+            <Grid item xs={10} xl={8} className="TipografiaLogo">
+              <LogoTop className="EnneagonLogo"/>
+              <EnneagonStudios className="EnneagonStudios" />
+            </Grid>
+            <Grid item xs={1} xl={2} className="UserAccessContainer">
+              <NavLink exact={true} activeClassName='is-active' className="link" to="/">Clientes</NavLink>
+              <NavLink exact={true} activeClassName='is-active' className="link" to="/timelog"><AccessTime className="icon"/></NavLink>
+              <NavLink exact={true} activeClassName='is-active' className="link" to="/"><AccountBox className="icon"/></NavLink>
+            </Grid>
+          </Grid>
+          <Grid item container xs={12} justify="center" className="NavContainer">
+            <Grid item lg={6}>
+              <nav>
+                <NavLink exact={true} activeClassName='is-active' className="link" to="/home">Home</NavLink>
+                <NavLink exact={true} activeClassName='is-active' className="link" to="/blog" >Blog</NavLink>
+                <NavLink exact={true} activeClassName='is-active' className="link" to="/contacto">Contacto</NavLink>
+              </nav>
+            </Grid>
+          </Grid>
+        </Grid>
+      </header>
+    );
+  }
+}
+
+export default Header;
---
+++ webapp/src/themes/Timelogs.theme.js
@@ -0,0 +1,65 @@
+import { createMuiTheme, responsiveFontSizes } from '@material-ui/core/styles';
+import { indigo, orange, lightGreen } from '@material-ui/core/colors';
+
+
+let TimelogsTheme = createMuiTheme({
+  palette: {
+    primary: indigo,
+    secondary: orange,
+    background: {
+      default: 'white',
+    },
+  },
+  overrides: {
+    MuiContainer: {
+      root: {
+        background: 'white',
+        padding: '11px 11px !important',
+      },
+    },
+    MuiTypography: {
+      h3: {
+        fontSize: '1.8rem !important',
+        marginBottom: 20
+      },
+      h4: {
+        fontSize: '1.6rem !important',
+        marginTop: 20
+      },
+    },
+    MuiCard: {
+      root: {
+        padding: 11,
+        width: 180,
+        position: 'relative',
+        background: lightGreen[50],
+      },
+    },
+    MuiCardContent: {
+      root: {
+        padding: 0,
+      },
+    },
+    MuiCardActions: {
+      root: {
+        position: 'absolute',
+        bottom: 0,
+        right: 0,
+        display: 'block',
+        textAlign: 'right',
+      },
+    },
+    // MuiIcon: {
+    //   root: {}
+    // }
+    MuiIconButton: {
+      root: {
+        fontSize: '1.8rem',
+      },
+    },
+  },
+});
+
+TimelogsTheme = responsiveFontSizes(TimelogsTheme);
+
+export default TimelogsTheme;
