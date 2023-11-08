f72fe952ee22df6b4517b1da372ddfe4fb021029
Alex <alebuteler@gmail.com>
2023-11-08 13:27:12 -0300

    Removed all MUI dependencies because they were conflicting with Tailwind when running `yarn start` (although there was no conflict when running `yarn dev`), in favor of just adding the 4 MUI svg icons I needed. Also got rid of a weird z-index issue affecting the link, by leaning more cleverly on the 'group' functionality that Tw provides.

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,30 +1,35 @@
 import Image from 'next/image'
 import Link from 'next/link'
-import { Laptop, Tablet, Smartphone, PresentToAll } from '@mui/icons-material'
 
 export default function Landing() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-evenly p-24">
-      <div className="relative z-0 flex flex-col place-items-center before:absolute before:h-[300px] before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#577ce2] after:dark:opacity-40 before:lg:h-[360px] z-[-1]">
-        <Image
-          src="/logo_landing.svg"
-          alt="Enneagon Studios Logo"
-          width={180}
-          height={180}
-          priority
-        />
-        <div className="relative z-10 flex gap-7 place-items-end ml-14 pt-6">
-          <Laptop className="text-5xl" />
-          <Tablet className="text-4xl mb-1" />
-          <Smartphone className="text-3xl mb-2" />
-          <Link className="link" href="/soon">
-            <div className="group absolute bottom-0 right-0 h-64 w-64 flex place-items-end justify-end">
-              <PresentToAll className="pb-10 mr-5 h-auto transition-[padding-bottom] ease duration-700 group-hover:pb-52" />
-            </div>
-          </Link>
-          <div className="text-2xl animate-blink">_</div>
+      <Link className="group" href="/soon">
+        <div className="relative flex flex-col place-items-center before:absolute before:h-[300px] before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#577ce2] after:dark:opacity-40 before:lg:h-[360px] z-[-1]">
+          <Image
+            src="/logo_landing.svg"
+            alt="Enneagon Studios Logo"
+            width={180}
+            height={180}
+            priority
+          />
+          <div className="relative flex gap-8 place-items-end ml-12 pt-8">
+            <Image src="/mui_laptop.svg" alt="Laptop icon" width={45} height={45} priority />
+            <Image src="/mui_tablet.svg" alt="Tablet icon" width={34} height={34} priority />
+            <Image src="/mui_smartphone.svg" alt="Smartphone icon" width={16} height={16} className="pb-1" priority />
+            <div className="w-5 h-6 invisible"></div>
+            <Image
+              src="/mui_present_to_all.svg"
+              alt="Present to all icon"
+              width={24}
+              height={24}
+              className="absolute right-7 mb-10 transition-[margin-bottom] ease duration-700 group-hover:mb-52"
+              priority
+            />
+            <span className="text-xl animate-blink">_</span>
+          </div>
         </div>
-      </div>
+      </Link>
     </main>
   )
 }
