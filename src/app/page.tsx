6a6c45a96eb1e2e90c404995ef7fc344f8842ded
Alex <alebuteler@gmail.com>
2023-11-08 11:24:27 -0300

    Coming soon completed; responsive.

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,7 +5,7 @@ import { Laptop, Tablet, Smartphone, PresentToAll } from '@mui/icons-material'
 export default function Landing() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-evenly p-24">
-      <div className="relative z-10 flex flex-col place-items-center before:absolute before:h-[300px] before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#577ce2] after:dark:opacity-40 before:lg:h-[360px] z-[-1]">
+      <div className="relative z-0 flex flex-col place-items-center before:absolute before:h-[300px] before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#577ce2] after:dark:opacity-40 before:lg:h-[360px] z-[-1]">
         <Image
           src="/logo_landing.svg"
           alt="Enneagon Studios Logo"
@@ -13,7 +13,7 @@ export default function Landing() {
           height={180}
           priority
         />
-        <div className="relative z-20 flex gap-7 place-items-end ml-14 pt-6">
+        <div className="relative z-10 flex gap-7 place-items-end ml-14 pt-6">
           <Laptop className="text-5xl" />
           <Tablet className="text-4xl mb-1" />
           <Smartphone className="text-3xl mb-2" />
--- src/app/soon/page.tsx
+++ src/app/soon/page.tsx
@@ -2,8 +2,8 @@ import Image from 'next/image'
 
 export default function ComingSoon() {
   return(
-    <main className="flex min-h-screen flex-col items-center justify-evenly p-24">
-      <div className="relative flex flex-col place-items-center before:absolute before:h-[300px] before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#577ce2] after:dark:opacity-40 before:lg:h-[360px] z-[-1]">
+    <main className="flex min-h-screen flex-col items-center justify-evenly p-8 md:p-24">
+      <div className="relative flex flex-col gap-2 place-items-center before:absolute before:h-[300px] before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#577ce2] after:dark:opacity-40 before:lg:h-[360px] z-[-1]">
         <Image
           src="/logo_landing.svg"
           alt="Enneagon Studios Logo"
@@ -13,15 +13,22 @@ export default function ComingSoon() {
           priority
         />
         <Image
-          src="/company_title_2023.svg"
+          src="/company_title.svg"
           alt="company title"
-          width={433}
-          height={90}
+          width={294}
+          height={50}
           priority
         />
-        <div className="relative text-xl flex gap-2 place-items-center pt-6">
+        <Image
+          src="/company_subtitle.svg"
+          alt="company title"
+          width={420}
+          height={25}
+          priority
+        />
+        <div className="relative md:text-xl flex place-items-center pt-3 md:pt-4">
           Coming soon
-          <div className="inline animate-blink">_</div>
+          <span className="ml-1 animate-blink">_</span>
         </div>
       </div>
     </main>
